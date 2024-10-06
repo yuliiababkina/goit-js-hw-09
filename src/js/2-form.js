244fac7d638d6f6129d570944d647218bbd75ffd
Yuliia Babkina <yulichka.babkina1312@gmail.com>
2024-10-06 20:55:18 +0300

    goit-js-hw-09

---
+++ src/js/2-form.js
@@ -0,0 +1,37 @@
+let formData = {};
+const form = document.querySelector('form');
+const localStorageKey = localStorage;
+
+//Отримуємо дані з input та додаємо їх до локального сховища
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value.trim();
+  localStorageKey.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+//Зчитуємо дані з локального сховища
+
+if (localStorageKey.getItem('feedback-form-state')) {
+  formData = JSON.parse(localStorageKey.getItem('feedback-form-state'));
+
+  for (const key in formData) {
+    form.elements[key].value = formData[key];
+  }
+}
+
+// Відправка форми
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  const email = form.elements.email.value.trim();
+  const msg = form.elements.message.value.trim();
+
+  if (email === '' || msg === '') {
+    return alert('Fill please all fields');
+  } else {
+    event.preventDefault();
+    console.log(formData);
+    event.currentTarget.reset();
+    localStorageKey.clear();
+    formData = {};
+  }
+}
