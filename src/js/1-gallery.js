c91ad04f51923ddf1c1959f4ef4013bd3026ddbd
Yuliia Babkina <yulichka.babkina1312@gmail.com>
2024-10-06 21:06:24 +0300

    updated goit-js-hw-09

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,4 +1,4 @@
-import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const images = [
